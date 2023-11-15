0d3421711dfbd14c97a1eb961f6ff28c0d180639
LeVequ <77817281+LeVequ@users.noreply.github.com>
2023-11-15 20:26:51 +0800

    Open Contributions (#5)

---
+++ include/main.h
@@ -0,0 +1,10 @@
+//
+// Created by levequ on 11/15/23.
+//
+
+#ifndef FPROJ_MAIN_H
+#define FPROJ_MAIN_H
+
+#include <iostream>
+
+#endif //FPROJ_MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,8 @@
-#include <iostream>
-#include "../include/menu.h"
+#include "../include/main.h"
 
 int main()
 {
-
-//    Menu panel = Menu(10,
-//                      9,
-//                      "LRT (Computer Automated Ticketing System)",
-//                      {"1. Single Journey Ticket",
-//                       "2. Add Beep Card Load"});
-
-    Menu panel = Menu(10,
-                      8,
-                      "Welcome to TCATS",
-//                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat orci imperdiet, fringilla augue id, vehicula tellus.");
-                      "Test a longer message needed lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-//                    "test");
-
-    panel.Print();
+    // For now, only use this file to test functions, do not explicitly write code here.
 
     return 0;
 }
