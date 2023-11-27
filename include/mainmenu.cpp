9965532d8160d6462b2dd3ff9b96593a9dd84819
LeVequ <77817281+LeVequ@users.noreply.github.com>
2023-11-27 09:10:51 +0000

    move mainmenu declerations to header

--- include/mainmenu.cpp
+++ include/mainmenu.cpp
@@ -1,92 +1,7 @@
-#include <iostream>
-#include <string>
 #include "mainmenu.h"
+#include "login.h"
+#include "stationselect.h"
 using namespace std;
-
-void ticket(){
-    string lrt[] = {"Roosevelt", "Balintawak", "Yamaha Monumento", "5th Avenue", "R. Papa", "Abad Santos", "Bluementritt", "Tayuman", "Bambang", "Doroteo Jose", "Carriedo", "Central Terminal", "United Nations", "Pedro Gil", "Quirino", "Vito Cruz", "Gil Puyat", "Libertad", "EDSA", "Baclaran"};
-    string lrt2[] = {"Recto", "Legarda", "Pureza", "V. Mapa", "J. Ruiz", "Gilmore", "Betty Go", "Cubao", "Anonas", "Katipunan", "Santolan", "Marikina-Pasig", "Antipolo"};
-    string mrt[] = {"North Avenue", "Quezon Avenue", "Kamuning", "Araneta Center (Cubao)", "Santolan (Annapolis)", "Ortigas", "Shaw Boulevard", "Boni", "Guadalupe", "Buendia", "Ayala", "Magallanes", "Taft Avenue"};
-    int lrt2Count = 13;
-    int lrtCount = 20;
-    int mrtCount = 13;
-    int line, orig = 0, selec = 1;
-
-    do{
-        cout << endl << "Select a train line: "<< endl;
-        cout << "[1] LRT-1"<< endl;
-        cout << "[2] LRT-2"<< endl;
-        cout << "[3] MRT"<< endl;
-        cout << "Insert Line Number: ";
-        cin >> line;
-        switch(line){
-        case 1:
-            cout << endl << "LRT-1 Destinations: " << endl;
-            for (int x=0; x<lrtCount; x++){
-                cout << x + 1 << ". " << lrt[x] << endl;
-            }
-            cout << "Insert Station Number: ";
-            cin >> selec;
-
-            cout << endl << "Select your current station: " << endl;
-            for (int x=0; x<lrtCount; x++){
-                cout << x + 1 << ". " << lrt[x] << endl;
-            }
-            cout << "Insert Station Number: ";
-            cin >> orig;
-            if (orig != selec){
-                cout << endl << "Riding Train To: " << lrt[selec-1] << endl;
-                cout << "Riding Train From: " << lrt[orig-1] << endl;
-            }
-            break;
-        case 2:
-            cout << endl << "LRT-2 Destinations: " << endl;
-            for (int y=0; y<lrt2Count; y++){
-                cout << y + 1 << ". " << lrt2[y] << endl;
-            }
-            cout << "Insert Station Number: ";
-            cin >> selec;
-
-            cout << endl << "Select your current station: " << endl;
-            for (int y=0; y<lrt2Count; y++){
-                cout << y + 1 << ". " << lrt2[y] << endl;
-            }
-            cout << "Insert Station Number: ";
-            cin >> orig;
-            if (orig != selec){
-                cout << endl << "Riding Train To: " << lrt2[selec-1] << endl;
-                cout << "Riding Train From: " << lrt2[orig-1] << endl;
-            }
-            break;
-        case 3:
-            cout << endl << "MRT Destinations: " << endl;
-            for (int z=0; z<mrtCount; z++){
-                cout << z + 1 << ". " << mrt[z] << endl;
-            }
-            cout << "Insert Station Number: ";
-            cin >> selec;
-
-            cout << endl << "Select your current station: " << endl;
-            for (int z=0; z<mrtCount; z++){
-                cout << z + 1 << ". " << mrt[z] << endl;
-            }
-            cout << "Insert Station Number: ";
-            cin >> orig;
-            if (orig != selec){
-                cout << endl << "Riding Train To: " << mrt[selec-1] << endl;
-                cout << "Riding Train From: " << mrt[orig-1] << endl;
-            }
-            break;
-        default:
-            cout << "Invalid Option" << endl;
-            break;
-        }
-        if (orig == selec){
-            cout << "DESTINATION CANNOT BE THE SAME WITH CURRENT STATION! (ERROR)" << endl << endl;
-        }
-    }while (orig == selec || line > 3);
-    
-}
 
 void MainMenu(){
     int options;
@@ -101,7 +16,7 @@ void MainMenu(){
     cin >> options;
     switch (options){
         case 1: 
-            ticket(); 
+            StationSelect();
             break;
         case 2: 
             reload();
@@ -110,6 +25,7 @@ void MainMenu(){
             cout << "DON'T EXPECT BEEP CARDS EXIST IN THIS TRANSACTION..."; 
             break;
         case 4: 
+            Login();
             break;
         default: 
             cout << "Invalid Option" << endl;
--- include/mainmenu.h
+++ include/mainmenu.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+
 void ticket();
 void reload();
 void MainMenu();
